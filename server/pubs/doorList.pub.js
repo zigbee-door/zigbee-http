303483d43e53d336b02941adce4f42ef023c6753
ziyi2 <673191402@qq.com>
2016-12-31 10:25:01 +0800

    进程存活检测预言

--- server/pubs/doorList.pub.js
+++ server/pubs/doorList.pub.js
@@ -9,12 +9,9 @@ const cmd_con = require('../constants/cmd.constant')
 
 module.exports = (data) => {
     //注意data需要转化为字符串发送
-    try {
-        redis_pub.publish(redis_con.doorList_send,JSON.stringify(data));   //发布订阅不能使用同一个redis对象
-        //Redis中的订阅发布模式, 当没有订阅者时, 消息会被直接丢弃(Redis不会持久化保存消息)
-    } catch(e) {
-        console.log("redis error",e);
-    }
+
+    redis_pub.publish(redis_con.doorList_send,JSON.stringify(data));   //发布订阅不能使用同一个redis对象
+
 
 
 };
